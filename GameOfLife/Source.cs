edd53fe7dfa43250cd6ec3262e1e02d266bae0a0
Jennifer Stone <jennifervstone@gmail.com>
2019-12-10 22:33:36 -0500

    Fixed Source

--- GameOfLife/Source.cs
+++ GameOfLife/Source.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Text;
-
+using System.Linq;
 namespace GameOfLife
 {
     class Source<T> : BaseCell where T :BaseCell, new()
@@ -11,11 +11,11 @@ namespace GameOfLife
         {
             return new T().GetColor();
         }
-
+     
         public override void StepForward(int x, int y, BaseCell[,] grid, BaseCell[,] nextGrid)
         {
-            
-            if(grid[x, y+1] == null)
+
+            if (grid[x, y + 1] == null || grid[x, y + 1] is OffCell)
             {
                 nextGrid[x, y + 1] = new T();
             }
