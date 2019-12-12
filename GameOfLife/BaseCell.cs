4a23f0a309c4536bbd0b499e35d5949bc6595781
blackboxlogic <blackboxlogic@gmail.com>
2019-12-12 16:45:47 -0500

    Removed duplicate Water cell

--- GameOfLife/BaseCell.cs
+++ GameOfLife/BaseCell.cs
@@ -4,18 +4,6 @@ using System.Drawing;
 
 namespace GameOfLife
 {
-	public class Water : BaseCell
-	{
-		public override Color GetColor()
-		{
-			throw new NotImplementedException();
-		}
-
-		public override void StepForward(int x, int y, BaseCell[,] grid, BaseCell[,] nextGrid)
-		{
-			throw new NotImplementedException();
-		}
-	} // delete me
 	public class Fire : BaseCell
 	{
 		public override Color GetColor()
