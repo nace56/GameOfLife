e9e7d9f17de9bd142896da3d091da714707cf202
Fazil Shaikh <fazil.shaikh@maine.edu>
2019-12-12 16:52:14 -0500

    created ice element

--- GameOfLife/BaseCell.cs
+++ GameOfLife/BaseCell.cs
@@ -4,20 +4,60 @@ using System.Drawing;
 
 namespace GameOfLife
 {
-	abstract class BaseCell
+	public class Water : BaseCell
+	{
+		public override Color GetColor()
+		{
+			throw new NotImplementedException();
+		}
+
+		public override void StepForward(int x, int y, BaseCell[,] grid, BaseCell[,] nextGrid)
+		{
+			throw new NotImplementedException();
+		}
+	} // delete me
+	public class Fire : BaseCell
+	{
+		public override Color GetColor()
+		{
+			throw new NotImplementedException();
+		}
+
+		public override void StepForward(int x, int y, BaseCell[,] grid, BaseCell[,] nextGrid)
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+	/*public class Ice : BaseCell
+	{
+		public override Color GetColor()
+		{
+			throw new NotImplementedException();
+		}
+
+		public override void StepForward(int x, int y, BaseCell[,] grid, BaseCell[,] nextGrid)
+		{
+			throw new NotImplementedException();
+		}
+	}*/
+
+	public abstract class BaseCell
 	{
 		public abstract void StepForward(int x, int y, BaseCell[,] grid, BaseCell[,] nextGrid);
 
 		public abstract Color GetColor();
 
-		public IEnumerable<BaseCell> GetNeighbors(int x, int y, BaseCell[,] grid)
+		public IEnumerable<BaseCell> GetNeighbors(int x, int y, BaseCell[,] grid,
+			Action<int, int, BaseCell> action = null)
 		{
-			for (int i = - 1; i < + 2; i++)
+			for (int i = -1; i < +2; i++)
 			{
-				for (int j = - 1; j < + 2; j++)
+				for (int j = -1; j < +2; j++)
 				{
 					if (i != 0 || j != 0)
 					{
+						if (action != null) action(i + x, j + y, grid[i + x, j + y]);
 						yield return grid[i + x, j + y];
 					}
 				}
@@ -25,3 +65,4 @@ namespace GameOfLife
 		}
 	}
 }
+ 
---
+++ GameOfLife/Ice.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Drawing;
+
+namespace GameOfLife
+{
+	class Ice : BaseCell
+	{
+		public override void StepForward(int x, int y, BaseCell[,] oldGrid, BaseCell[,] nextGrid)
+		{
+			var neighborCells = GetNeighbors(x, y, oldGrid, (i, j, c) => new Ice());
+		}
+
+		public override Color GetColor()
+		{
+			return Color.FromArgb(255, 0, 255, 255);
+		}
+	}
+
+}
